da7a405f4d331ec71864d905312013e73486dba9
Myles Scolnick <mscolnick@gmail.com>
2023-04-13 17:03:02 -0400

    first commit

---
+++ CONTRIBUTING.md
@@ -0,0 +1,49 @@
+# Contributing
+
+Install the dependencies
+
+```bash
+$ yarn install
+```
+
+Build the typescript and package it for distribution
+
+```bash
+$ yarn dist
+```
+
+Run the tests :heavy_check_mark:
+
+```bash
+$ yarn test
+...
+```
+
+## Publish to a distribution branch
+
+Actions are run from GitHub repos so we will checkin the packed dist folder.
+
+Then run [ncc](https://github.com/zeit/ncc) and push the results:
+
+```bash
+$ yarn dist
+$ git add dist
+$ git commit -a -m "prod dependencies"
+$ git push origin releases/v1
+```
+
+Note: We recommend using the `--license` option for ncc, which will create a license file for all of the production node modules used in your project.
+
+Your action is now published! :rocket:
+
+See the [versioning documentation](https://github.com/actions/toolkit/blob/master/docs/action-versioning.md)
+
+## Validate
+
+You can now validate the action by referencing `./` in a workflow in your repo (see [test.yml](.github/workflows/test.yml))
+
+```yaml
+uses: ./
+```
+
+See the [actions tab](https://github.com/turbocache-build/turbocache-action/actions) for runs of this action! :rocket:
---
+++ src/main.ts
@@ -0,0 +1,70 @@
+import * as core from "@actions/core";
+import { readFileSync } from "fs";
+import { findTurboSummaryFile } from "./findFile";
+
+async function run(): Promise<void> {
+  try {
+    core.debug("Starting Turbocache Action");
+
+    const token = process.env.TURBO_TOKEN;
+    const team = process.env.TURBO_TEAM;
+    if (!token) {
+      core.error("TURBO_TOKEN is not set.");
+      core.warning("Skipping Turbocache Action.");
+      return;
+    }
+    if (!team) {
+      core.error("TURBO_TEAM is not set.");
+      core.warning("Skipping Turbocache Action.");
+      return;
+    }
+
+    if (!process.env.TURBO_RUN_SUMMARY) {
+      core.error(
+        "TURBO_RUN_SUMMARY is not set. Please add 'TURBO_RUN_SUMMARY: true' to your workflow's env section."
+      );
+      core.warning("Skipping Turbocache Action.");
+      return;
+    }
+
+    const file = findTurboSummaryFile();
+    if (!file) {
+      core.error("No Turbo summary file found.");
+      core.warning("Skipping Turbocache Action.");
+      return;
+    }
+
+    core.debug(`Found Turbo summary file: ${file}`);
+
+    const uploadUrl = `https://cache.turbocache.build/api/v1/runs?teamId=${team}`;
+    core.debug(`Uploading to ${uploadUrl}`);
+
+    const summary = JSON.parse(readFileSync(file, "utf8"));
+
+    await fetch(uploadUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        summary: summary,
+        teamId: team,
+        repo: process.env.GITHUB_REPOSITORY,
+        sha: process.env.GITHUB_SHA,
+        branch: process.env.GITHUB_REF,
+        workflow: process.env.GITHUB_WORKFLOW,
+        runId: process.env.GITHUB_RUN_ID,
+      }),
+    });
+
+    core.debug("Upload complete");
+  } catch (error) {
+    if (error instanceof Error) {
+      core.error("Error uploading to Turbocache");
+      core.error(error.message);
+    }
+  }
+}
+
+run();
